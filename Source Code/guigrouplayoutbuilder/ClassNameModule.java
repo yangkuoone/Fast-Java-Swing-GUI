708892f6d0a37cdf0a005e6bf9f70ad707dd4f96
David McKay <DavidScript@users.noreply.github.com>
2015-01-13 14:40:58 -0700

    Update ClassNameModule.java

--- Source Code/guigrouplayoutbuilder/ClassNameModule.java
+++ Source Code/guigrouplayoutbuilder/ClassNameModule.java
@@ -9,16 +9,16 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class ClassNameModule extends JPanel{
-
+	
 	//Instance Variables:( Automated )
 	//--------------------------------------------------------------------------|
-
+	
 	private JTextField classNameField;
 	private JTextField extendsField;
 	private JTextField implementsField;
-   
-   final public Dimension textAreaDimension = new Dimension( 496, 25);
-   
+	
+	final public Dimension textAreaDimension = new Dimension( 496, 25);
+	
 	//--------------------------------------------------------------------------|
 
 	public ClassNameModule( ){
@@ -49,21 +49,21 @@ public class ClassNameModule extends JPanel{
 
 		JLabel classNameLabel = new JLabel( "Name:" );
 		classNameField = new JTextField( );
-      classNameField.setMaximumSize( textAreaDimension );
-      classNameField.setMinimumSize( textAreaDimension );
-      classNameField.setFont( new Font("Lucida Console" , Font.PLAIN, 12) );
+                classNameField.setMaximumSize( textAreaDimension );
+      		classNameField.setMinimumSize( textAreaDimension );
+      		classNameField.setFont( new Font("Lucida Console" , Font.PLAIN, 12) );
       
 		JLabel extendsLabel = new JLabel( "Extends:" );
 		extendsField = new JTextField( );
-      extendsField.setMaximumSize( textAreaDimension );
-      extendsField.setMinimumSize( textAreaDimension );
-      extendsField.setFont( new Font("Lucida Console" , Font.PLAIN, 12) );
+      		extendsField.setMaximumSize( textAreaDimension );
+      		extendsField.setMinimumSize( textAreaDimension );
+      		extendsField.setFont( new Font("Lucida Console" , Font.PLAIN, 12) );
       
-      JLabel implementsLabel = new JLabel( "Implements:" );
+      		JLabel implementsLabel = new JLabel( "Implements:" );
 		implementsField = new JTextField( );
-      implementsField.setMaximumSize( textAreaDimension );
-      implementsField.setMinimumSize( textAreaDimension );
-      implementsField.setFont( new Font("Lucida Console" , Font.PLAIN, 12) );
+      		implementsField.setMaximumSize( textAreaDimension );
+      		implementsField.setMinimumSize( textAreaDimension );
+		implementsField.setFont( new Font("Lucida Console" , Font.PLAIN, 12) );
       
 		//-----------------------------------------------------------------------|
 
@@ -106,47 +106,47 @@ public class ClassNameModule extends JPanel{
 		layout.setVerticalGroup(vGroup);
 	}
    
-   /**
-   * This method will format the entered fields into the desired output.
-   */
-   @Override
-   public String toString(){
-      String outputStr = "public class ";
-      
-      outputStr += classNameField.getText();
-      
-      if( !extendsField.getText().equals( "" ) )
-         outputStr += " extends " + extendsField.getText();
-           
-      if( !implementsField.getText().equals( "" ) )
-         outputStr += " implements " + implementsField.getText();
-         
-      outputStr += "{\n";
-      
-      return( outputStr );
-   }
-   
-   public String getName( ){
-      return( classNameField.getText() );
-   }
-   
-   public void setName( String nameStr ){
-      classNameField.setText( nameStr );
-   }
-   
-   public String getExtends( ){
-      return( extendsField.getText() );
-   }
-   
-   public void setExtends( String extendsStr ){
-      extendsField.setText( extendsStr );
-   }
-   
-   public String getImplements( ){
-      return( implementsField.getText() );
-   }
-   
-   public void setImplements( String implementsStr ){
-      implementsField.setText( implementsStr );
-   }
+	/**
+	* This method will format the entered fields into the desired output.
+	*/
+	@Override
+	public String toString(){
+		String outputStr = "public class ";
+		
+		outputStr += classNameField.getText();
+		
+		if( !extendsField.getText().equals( "" ) )
+			outputStr += " extends " + extendsField.getText();
+		   
+		if( !implementsField.getText().equals( "" ) )
+			outputStr += " implements " + implementsField.getText();
+		 
+			outputStr += "{\n";
+		
+		return( outputStr );
+	}
+	
+	public String getName( ){
+		return( classNameField.getText() );
+	}
+	
+	public void setName( String nameStr ){
+		classNameField.setText( nameStr );
+	}
+	
+	public String getExtends( ){
+		return( extendsField.getText() );
+	}
+	
+	public void setExtends( String extendsStr ){
+		extendsField.setText( extendsStr );
+	}
+	
+	public String getImplements( ){
+		return( implementsField.getText() );
+	}
+	
+	public void setImplements( String implementsStr ){
+		implementsField.setText( implementsStr );
+	}
 }
